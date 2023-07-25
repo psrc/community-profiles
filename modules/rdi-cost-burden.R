160f81cc3df3fec22838b2fa06b0a400adc82b6a
cyyllam <cyyllam@gmail.com>
2023-07-25 14:56:48 -0700

    revised cost burden chart

--- modules/rdi-cost-burden.R
+++ modules/rdi-cost-burden.R
@@ -92,30 +92,38 @@ rdi_cost_burden_server <- function(id, shape, place) {
       #   select(description, renter_occupied, owner_occupied, ends_with('share'), ends_with('reg'))
     })
 
-    # plot_data <- reactive({
-    #   # data in long form for plotting
-    #   
-    #   # df <- bind_rows(data()) %>%
-    #   #   filter(description != 'All') %>%
-    #   #   select(chas_year, geography_name, description, ends_with('share')) %>%
-    #   #   pivot_longer(cols = ends_with('Share'),
-    #   #                names_to = 'type',
-    #   #                values_to = 'value')
-    # })
-    # 
-    # plot_clean_data <- reactive({
-    #   # munge long form data for visual
-    #   
-    #   # geog <- c(place(), 'Region')
-    #   # 
-    #   # desc_rev <- rev(unique(plot_data()$description))
-    #   # 
-    #   # plot_data() %>%
-    #   #   mutate(type_desc = case_when(type == 'rental_share' ~ 'Rental Households',
-    #   #                                type == 'owner_share' ~ 'Owner Households')) %>%
-    #   #   mutate(description = factor(description, levels = desc_rev)) %>%
-    #   #   arrange(description)
-    # })
+    plot_data <- reactive({
+      # data (shares) for renter and owner in long form for plotting
+
+      dfs <- map(data(), ~.x[['s']])
+      
+      pivot_table_longer <- function(table) {
+        table %>% 
+          filter(description %in% c(str_subset(description, "^Total.*"))) %>%
+          pivot_longer(cols = setdiff(colnames(table), c(vals$exc_cols, 'description')),
+                       names_to = 'race_ethnicity',
+                       values_to = 'value')
+      }
+      
+      dfs_share <- map(dfs, ~pivot_table_longer(.x))
+    })
+
+    plot_clean_data <- reactive({
+      # munge long form data for visual
+
+      filter_set_levels <- function(table) {
+        df <- table %>% 
+          filter(!race_ethnicity %in% c('POC', 'Total'))
+        
+        desc_rev <- rev(unique(df$race_ethnicity))
+        
+        df %>% 
+          mutate(race_ethnicity = factor(race_ethnicity, levels = desc_rev)) %>%
+          arrange(race_ethnicity)
+      }
+      
+      dfs <- map(plot_data(), ~filter_set_levels(.x))
+    })
     
     place_name <- reactive({unique(data()$r$e$geography_name)})
     
@@ -139,6 +147,8 @@ rdi_cost_burden_server <- function(id, shape, place) {
       ))
     })
     
+    # renter ----
+    
     output$r_e_table <- renderDT({
       # Renter Estimate table display
       
@@ -158,20 +168,39 @@ rdi_cost_burden_server <- function(id, shape, place) {
         formatPercentage(2:10, 1)
     })
 
-    # output$plot01 <- renderEcharts4r({
-    #   # echart_rdi(data = plot_clean_data(),
-    #   #            filter_type = "renter_share",
-    #   #            desc_col = description, 
-    #   #            str_wrap_num = 15,
-    #   #            group = geography_name,
-    #   #            x = 'description',
-    #   #            y = 'value',
-    #   #            title = 'Renter Households',
-    #   #            egrid_left = "30%")|>
-    #   #   e_legend(bottom=0) |>
-    #   #   e_group("grp")
-    # })
-    # 
+    output$r_plot01 <- renderEcharts4r({
+      
+      plot_clean_data()$r %>% 
+        mutate(race_ethnicity := str_wrap(race_ethnicity, 15)) %>%
+        group_by(description) %>%
+        e_charts_(x = 'race_ethnicity') |>
+        e_bar_('value') |>
+        e_y_axis(splitNumber = 3) |>
+        e_x_axis(axisLabel = list(interval = 0L),
+                 axisTick = list(alignWithLabel = TRUE)) |>
+        e_flip_coords() |>
+        e_grid(left = "20%", top = '10%') |>
+        e_title(text = "Renter Households",
+                left = 'center',
+                textStyle = list(fontSize = 12)) |>
+        e_color(psrc_colors$obgnpgy_5) |>
+        e_tooltip(formatter =  e_tooltip_item_formatter("percent", digits = 1)) |>
+        e_tooltip(formatter =  htmlwidgets::JS("
+                                           function(params, ticket, callback) {
+                                           var fmt = new Intl.NumberFormat('en', {\"style\":\"percent\",\"minimumFractionDigits\":1,\"maximumFractionDigits\":1,\"currency\":\"USD\"});\n        
+                                           var idx = 0;\n        
+                                           if (params.name == params.value[0]) {\n            
+                                           idx = 1;\n        }\n        
+                                           return(params.marker + ' ' +\n       
+                                                  params.seriesName + ': ' + fmt.format(parseFloat(params.value[idx]))
+                                                  )
+                                           }")
+        ) |>
+        e_x_axis(formatter = e_axis_formatter("percent", digits = 0))|>
+        e_legend(bottom=0) |>
+        e_group("grp")
+    })
+
     # output$plot02 <- renderEcharts4r({
     #   # echart_rdi(data = plot_clean_data(),
     #   #            filter_type = "owner_share",
