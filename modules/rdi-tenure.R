b54d05df5cf602a6fc68f290c41195961b166d48
cyyllam <cyyllam@gmail.com>
2023-08-21 20:55:16 -0700

    added graphs to rdi tenure

--- modules/rdi-tenure.R
+++ modules/rdi-tenure.R
@@ -9,9 +9,9 @@ rdi_tenure_ui <- function(id) {
              tabPanel(title = 'Renter',
                       div(style = "padding-top: 1rem;",
                           fluidRow(
-                            column(4,
+                            column(6,
                                    echarts4rOutput(ns('r_plot'))),
-                            column(4,
+                            column(6,
                                    leafletOutput(ns('r_map'))
                             )
                           )# end fluidrow
@@ -25,15 +25,17 @@ rdi_tenure_ui <- function(id) {
                       ) # end fluidRow
              ), # end tabPanel Renter 
              tabPanel(title = 'Owner',
+                      div(style = "padding-top: 1rem;",
                       fluidRow(
-                      column(4,
+                      column(6,
                              echarts4rOutput(ns('o_plot'))),
-                      column(4,
+                      column(6,
                              leafletOutput(ns('o_map')))
                       ), # end FluidRow
                       fluidRow(
                         column(width = 12,
                                uiOutput(ns('o_tableui'))
+                        )
                         )
                       ) # end fluidRow
              ) # end tabPanel
@@ -45,6 +47,9 @@ rdi_tenure_server <- function(id, shape, place) {
   
   moduleServer(id, function(input, output, session) { 
     ns <- session$ns
+    
+    vals <- reactiveValues(source = 'Sources: US HUD, 2015-2019 Comprehensive Housing Affordability Strategy (CHAS) Table 9',
+                           id_cols = c('chas_year', 'geography_name', 'description'))
     
     output$r_tableui <- renderUI({
       div(
@@ -84,13 +89,7 @@ rdi_tenure_server <- function(id, shape, place) {
     table_data <- reactive({
       # data in wide for display in table
       
-      # region <- data()$region %>%
-      #   select(description, renter_occupied, owner_occupied, ends_with('share')) %>%
-      #   rename_with(~paste0(.x, '_reg'))
-      # 
-      # d <- left_join(data()$place, region, by = c('description' = 'description_reg')) %>%
-      #   select(description, renter_occupied, owner_occupied, ends_with('share'), ends_with('reg'))
-      id_cols <- c('chas_year', 'geography_name', 'description')
+      # id_cols <- c('chas_year', 'geography_name', 'description')
       
       region <- data()$region %>%
         select(-chas_year, -geography_name) %>%
@@ -99,37 +98,32 @@ rdi_tenure_server <- function(id, shape, place) {
       d <- left_join(data()$place, region, by = c('description' = 'description_region'))
       
       rdf <- d %>% 
-        select(id_cols, starts_with('all'), starts_with('renter'))
+        select(vals$id_cols, starts_with('all'), starts_with('renter'))
       odf <- d %>% 
-        select(id_cols, starts_with('all'), starts_with('owner'))
+        select(vals$id_cols, starts_with('all'), starts_with('owner'))
       
       return(list(r = rdf, o = odf))
     })
     
-    # plot_data <- reactive({
-    #   # data in long form for plotting
-    #   
-    #   df <- bind_rows(data()) %>%
-    #     filter(description != 'All') %>%
-    #     select(chas_year, geography_name, description, ends_with('share')) %>%
-    #     pivot_longer(cols = ends_with('Share'),
-    #                  names_to = 'type',
-    #                  values_to = 'value')
-    # })
+    plot_data <- reactive({
+      # data in long form for plotting
+
+      df <- bind_rows(data()) %>% 
+        select(chas_year, geography_name, description, ends_with('share'))%>%
+        pivot_longer(cols = ends_with('Share'),
+                     names_to = 'type',
+                     values_to = 'value')
+      
+        geog <- c(place(), 'Region')
     
-    # plot_clean_data <- reactive({
-    #   # munge long form data for visual
-    #   
-    #   geog <- c(place(), 'Region')
-    # 
-    #   desc_rev <- rev(unique(plot_data()$description))
-    #   
-    #   plot_data() %>%
-    #     mutate(type_desc = case_when(type == 'rental_share' ~ 'Rental Households',
-    #                                  type == 'owner_share' ~ 'Owner Households')) %>%
-    #     mutate(description = factor(description, levels = desc_rev)) %>%
-    #     arrange(description)
-    # })
+        desc_rev <- rev(unique(df$description))
+
+        df %>%
+          mutate(type_desc = case_when(type == 'rental_share' ~ 'Rental Households',
+                                       type == 'owner_share' ~ 'Owner Households')) %>%
+          mutate(description = factor(description, levels = desc_rev)) %>%
+          arrange(description)
+    })
     
     place_name <- reactive({unique(data()$place$geography_name)})
     
@@ -154,8 +148,6 @@ rdi_tenure_server <- function(id, shape, place) {
     output$r_table <- renderDT({
       # table display
       
-      source <- 'Sources: US HUD, 2015-2019 Comprehensive Housing Affordability Strategy (CHAS) Table 9'
-      
       d <- table_data()$r %>% 
         select(-chas_year, -geography_name) %>% 
         select(description, renter_occupied, all_units, renter_share, renter_occupied_region, all_units_region, renter_share_region)
@@ -167,15 +159,13 @@ rdi_tenure_server <- function(id, shape, place) {
                                columnDefs = list(list(className = 'dt-center', targets = 1:6))),
                 caption = htmltools::tags$caption(
                   style = 'caption-side: bottom; text-align: right;',
-                  htmltools::em(source)
+                  htmltools::em(vals$source)
                 )) %>%
         formatPercentage(str_subset(colnames(d), ".*share(.)*$"), 1)
     })
     
     output$o_table <- renderDT({
       # table display
-      
-      source <- 'Sources: US HUD, 2015-2019 Comprehensive Housing Affordability Strategy (CHAS) Table 9'
       
       d <- table_data()$o %>% 
         select(-chas_year, -geography_name) %>% 
@@ -188,58 +178,42 @@ rdi_tenure_server <- function(id, shape, place) {
                                columnDefs = list(list(className = 'dt-center', targets = 1:6))),
                 caption = htmltools::tags$caption(
                   style = 'caption-side: bottom; text-align: right;',
-                  htmltools::em(source)
+                  htmltools::em(vals$source)
                 )) %>%
         formatPercentage(str_subset(colnames(d), ".*share(.)*$"), 1)
     })
     
-    # output$table <- renderDT({
-    #   # table display
-    #   
-    #   source <- 'Sources: US HUD, 2015-2019 Comprehensive Housing Affordability Strategy (CHAS) Table 9'
-    #   
-    #   datatable(table_data(),
-    #             container = container(),
-    #             rownames = FALSE,
-    #             options = list(dom = 'tipr',
-    #                            columnDefs = list(list(className = 'dt-center', targets = 1:8))),
-    #             caption = htmltools::tags$caption(
-    #               style = 'caption-side: bottom; text-align: right;',
-    #               htmltools::em(source)
-    #             )) %>%
-    #     formatPercentage(str_subset(colnames(table_data()), ".*share(.)*$"), 1)
-    # })
+    output$r_plot <- renderEcharts4r({
+      
+      echart_rdi(data = plot_data(),
+                 filter_type = "renter_share",
+                 desc_col = description,
+                 str_wrap_num = 20,
+                 group = geography_name,
+                 x = 'description',
+                 y = 'value',
+                 title = 'Renter Households',
+                 egrid_left = "20%")|>
+          e_legend(bottom=0) |>
+          e_group("grp")
+    })
     
-    # output$plot01 <- renderEcharts4r({
-    #   echart_rdi(data = plot_clean_data(),
-    #              filter_type = "renter_share",
-    #              desc_col = description, 
-    #              str_wrap_num = 15,
-    #              group = geography_name,
-    #              x = 'description',
-    #              y = 'value',
-    #              title = 'Renter Households',
-    #              egrid_left = "30%")|>
-    #       e_legend(bottom=0) |>
-    #       e_group("grp")
-    # })
-    
-    # output$plot02 <- renderEcharts4r({
-    #   echart_rdi(data = plot_clean_data(),
-    #              filter_type = "owner_share",
-    #              desc_col = description, 
-    #              str_wrap_num = 15,
-    #              group = geography_name,
-    #              x = 'description',
-    #              y = 'value',
-    #              title = 'Owner Households',
-    #              egrid_left = "30%")|>
-    #       e_legend(show=FALSE) |>
-    #       e_toolbox_feature("dataView") |>
-    #       e_toolbox_feature("saveAsImage") |>
-    #       e_group("grp") |>
-    #       e_connect_group("grp")
-    # })
+    output$o_plot <- renderEcharts4r({
+      echart_rdi(data = plot_data(),
+                 filter_type = "owner_share",
+                 desc_col = description,
+                 str_wrap_num = 20,
+                 group = geography_name,
+                 x = 'description',
+                 y = 'value',
+                 title = 'Owner Households',
+                 egrid_left = "20%")|>
+          e_legend(show=FALSE) |>
+          e_toolbox_feature("dataView") |>
+          e_toolbox_feature("saveAsImage") |>
+          e_group("grp") |>
+          e_connect_group("grp")
+    })
     
     map_data <- reactive({
       s <- shape %>% filter(geog_name == place())
